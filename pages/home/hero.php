b90be7de951c16aeadb10bf90b85b23b36892f40
Edaurdo <barrosoeduardo64@gmail.com>
2023-08-17 14:59:18 -0300

    Adicionando conteúdo na página

--- pages/home/hero.php
+++ pages/home/hero.php
@@ -1,6 +1,17 @@
 <div class="module--hero" id="hero">
-    <h1>Sla123123</h1>
-    edu
-    <?= get_field("hero-title");?>
-    <div><?= get_field('hero-title')?></div>
+    <div class="module--hero__content">
+        <div class="module--hero__title"><?= get_field('hero-title')?></div>
+            <div class="module--hero__text-1"><?= get_field('hero-text-1')?></div>
+            <div class="module--hero__text-2"><?= get_field('hero-text-2')?></div>
+            <div class="module--hero__text-3"><?= get_field('hero-text-3')?></div>
+
+    </div>
+
+    <img src="https://ibb.co/YLwm08C" alt="" class="module--hero__small-cicle-1">
+    <img src="https://ibb.co/NjyhxC0" alt="" class="module--hero__small-cicle-2">
+    <img src="https://ibb.co/YXkyqJ5" alt="" class="module--hero__background-cicles">
+    <img src="https://ibb.co/Gxf9dRc" alt="" class="module--hero__right-cicle">
+    <img src="https://ibb.co/MC37jbW" alt="" class="module--hero__star-4-legs">
+    <img src="https://ibb.co/rfvW2Qn" alt="" class="module--hero__star-6-legs">
+    <img src="https://ibb.co/JRYJC4p" alt="" class="module--hero__bg-gradient">
 </div>
